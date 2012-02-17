7d0d5c6914d89f0b2daab3d521cd7bdefdf49bb8
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-02-17 15:19:48 +0000

    NumberValueImpl.java: implemented equals() and hashCode() methods.

--- trunk/org.pnml.tools.epnk.pntypes.hlpng.runtime/src/numberRuntime/impl/NumberValueImpl.java
+++ trunk/org.pnml.tools.epnk.pntypes.hlpng.runtime/src/numberRuntime/impl/NumberValueImpl.java
@@ -252,4 +252,28 @@ public abstract class NumberValueImpl extends AbstractValueImpl implements Numbe
         return result.toString();
     }
 
+	@Override
+    public int hashCode()
+    {
+	    final int prime = 31;
+	    int result = 1;
+	    result = prime * result + n;
+	    return result;
+    }
+
+	@Override
+    public boolean equals(Object obj)
+    {
+	    if(this == obj)
+		    return true;
+	    if(obj == null)
+		    return false;
+	    if(getClass() != obj.getClass())
+		    return false;
+	    NumberValueImpl other = (NumberValueImpl) obj;
+	    if(n != other.n)
+		    return false;
+	    return true;
+    }
+
 } //NumberValueImpl
