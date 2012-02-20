7e93d28a39a46341102a2d59b72e31f77d8b5854
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-02-20 14:24:19 +0000

    RT: Test: MSValueTest.java: various tests (MS in products, products in MS etc.)

--- org.pnml.tools.epnk.pntypes.hlpng.runtime.tests/src/runtime/tests/MSValueTest.java
+++ org.pnml.tools.epnk.pntypes.hlpng.runtime.tests/src/runtime/tests/MSValueTest.java
@@ -6,6 +6,11 @@
  */
 package runtime.tests;
 
+import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.integers.IntegersFactory;
+import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.TermsFactory;
+
+import productRuntime.ProductValue;
+import productRuntime.ProductruntimeFactory;
 import numberRuntime.IntValue;
 import numberRuntime.NumberruntimeFactory;
 
@@ -85,6 +90,7 @@ public class MSValueTest extends AbstractValueTest
         {
         	intValue9 = NumberruntimeFactory.eINSTANCE.createIntValue();
             intValue9.setN(9);
+            intValue9.setSort(IntegersFactory.eINSTANCE.createInteger());
         }
         getFixture().add(intValue9, multiplicity);
         
@@ -92,15 +98,18 @@ public class MSValueTest extends AbstractValueTest
         	msAppend = RuntimeFactory.eINSTANCE.createMSValue();
         	intAppend9 = NumberruntimeFactory.eINSTANCE.createIntValue();
         	intAppend9.setN(9);
+        	intAppend9.setSort(IntegersFactory.eINSTANCE.createInteger());
         	msAppend.add(intAppend9, 1);
         	
         	intAppend19 = NumberruntimeFactory.eINSTANCE.createIntValue();
         	intAppend19.setN(19);
+        	intAppend19.setSort(IntegersFactory.eINSTANCE.createInteger());
         	msAppend.add(intAppend19, 1);
         }
         {
         	tmpValue10 = NumberruntimeFactory.eINSTANCE.createIntValue();
         	tmpValue10.setN(10);
+        	tmpValue10.setSort(IntegersFactory.eINSTANCE.createInteger());
         }
     }
 
@@ -145,9 +154,103 @@ public class MSValueTest extends AbstractValueTest
      */
     public void testAdd__AbstractValue_int()
     {
-    	getFixture().add(tmpValue10, 1);
-        
-        assertEquals(1, getFixture().getMultiplicity(tmpValue10));
+    	// simple number
+    	{
+    		getFixture().add(tmpValue10, 1);
+            assertEquals(1, getFixture().getMultiplicity(tmpValue10));
+    	}
+    	// products
+    	{
+    		MSValue msValue = RuntimeFactory.eINSTANCE.createMSValue();
+    		ProductValue p1 = createProductValue(5, 6);
+    		ProductValue p2 = createProductValue(5, 6);
+    		ProductValue p3 = createProductValue(7, 8);
+    		
+    		ProductValue pTest1 = createProductValue(5, 6);
+    		ProductValue pTest2 = createProductValue(7, 8);
+    		
+    		msValue.add(p1, 1);
+    		msValue.add(p2, 2);
+    		msValue.add(p3, 1);
+    		
+    		assertEquals(3, msValue.getMultiplicity(pTest1));
+    		assertEquals(1, msValue.getMultiplicity(pTest2));
+    	}
+    	// multisets of products
+    	{
+    		MSValue msValue = RuntimeFactory.eINSTANCE.createMSValue();
+    		{
+        		MSValue v1 = createMSValue(6, 5);
+        		msValue.add(v1, 1);
+        		MSValue v2 = createMSValue(6, 5);
+        		msValue.add(v2, 2);
+        		MSValue v22 = createMSValue(5, 6);
+        		msValue.add(v22, 1);
+        		
+        		MSValue test1 = createMSValue(6, 5);
+        		assertEquals(4, msValue.getMultiplicity(test1));
+    		}
+    		{
+        		MSValue v3 = createMSValue(10, 15);
+        		msValue.add(v3, 5);
+        		
+        		MSValue test2 = createMSValue(10, 15);
+
+        		assertEquals(5, msValue.getMultiplicity(test2));	
+    		}
+    	}
+    	// products of multisets of products
+    	{
+    		MSValue msValue = RuntimeFactory.eINSTANCE.createMSValue();
+    		{
+    			ProductValue pValue = ProductruntimeFactory.eINSTANCE.createProductValue();
+    			pValue.setSort(TermsFactory.eINSTANCE.createProductSort());
+    			pValue.getComponents().add(createMSValue(4, 5));
+    			msValue.add(pValue, 1);
+    			
+    		}
+    		{
+    			ProductValue pValue = ProductruntimeFactory.eINSTANCE.createProductValue();
+    			pValue.setSort(TermsFactory.eINSTANCE.createProductSort());
+    			pValue.getComponents().add(createMSValue(4, 5));
+    			msValue.add(pValue, 2);
+    		}
+    		{
+    			ProductValue pValue = ProductruntimeFactory.eINSTANCE.createProductValue();
+    			pValue.setSort(TermsFactory.eINSTANCE.createProductSort());
+    			pValue.getComponents().add(createMSValue(5, 4));
+    			
+    			assertEquals(3, msValue.getMultiplicity(pValue));
+    			assertFalse(3 != msValue.getMultiplicity(pValue));
+    		}
+    	}
+    }
+    
+    private static MSValue createMSValue(int a, int b)
+    {
+    	MSValue msValue = RuntimeFactory.eINSTANCE.createMSValue();
+    	//msValue.setSort(TermsFactory.eINSTANCE.createMultiSetSort());
+    	msValue.add(createProductValue(a, b), 1);
+		msValue.add(createProductValue(b, a), 1);
+
+		return msValue;
+    }
+    
+    private static ProductValue createProductValue(int a, int b)
+    {
+    	ProductValue pValue = ProductruntimeFactory.eINSTANCE.createProductValue();
+		pValue.setSort(TermsFactory.eINSTANCE.createProductSort());
+		{
+			IntValue v = NumberruntimeFactory.eINSTANCE.createIntValue();
+			v.setN(a);
+			pValue.getComponents().add(v);
+		}
+		{
+			IntValue v = NumberruntimeFactory.eINSTANCE.createIntValue();
+			v.setN(b);
+			pValue.getComponents().add(v);
+		}
+		return pValue;
     }
 
     /**
@@ -160,11 +263,9 @@ public class MSValueTest extends AbstractValueTest
     public void testAppend__MSValue()
     {
     	getFixture().append(msAppend);
-    	
+  
     	assertTrue(multiplicity + 1 == 
     			getFixture().getValues().get(intAppend9));
-    	
-    	assertTrue(1 == getFixture().getValues().get(intAppend19));
     }
 
 } //MSValueTest
