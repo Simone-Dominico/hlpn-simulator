ee3ae0c36c29e75cd1a9c653fd6aa9250f3cf4b4
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-02-28 08:57:06 +0000

    Simulator: extracted super types for PopupMenuCategory.java & PopupMenuItem.java - AbstractMenuCategory.java & AbstractMenuItem.java. IActionProvider.java PopupMenu.java RectangleOverlay.java

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/view/PopupMenuItem.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/view/PopupMenuItem.java
@@ -1,22 +1,10 @@
 package org.pnml.tools.epnk.applications.hlpng.view;
 
-public class PopupMenuItem
+public class PopupMenuItem extends AbstractMenuItem
 {
-	protected String name = null;
-
 	public PopupMenuItem(String name)
 	{
-		this.name = name;
+		super(name);
 	}
-	
-	public String getName()
-    {
-    	return name;
-    }
-
-	public void setName(String name)
-    {
-    	this.name = name;
-    }
 	
 }
