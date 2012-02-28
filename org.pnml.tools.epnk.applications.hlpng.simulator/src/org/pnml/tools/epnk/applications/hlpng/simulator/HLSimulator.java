edaf8f7806833db45b9fe57fe5df7118a2464b7b
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-02-28 08:17:10 +0000

    Simulator: HLSimulator.java & SimulatorPresentationManager.java

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/HLSimulator.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/HLSimulator.java
@@ -3,11 +3,14 @@ package org.pnml.tools.epnk.applications.hlpng.simulator;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.jface.action.Action;
+import org.pnml.tools.epnk.annotations.manager.IPresentationManager;
 import org.pnml.tools.epnk.annotations.netannotations.NetAnnotation;
 import org.pnml.tools.epnk.annotations.netannotations.NetAnnotations;
 import org.pnml.tools.epnk.annotations.netannotations.NetannotationsFactory;
 import org.pnml.tools.epnk.annotations.netannotations.ObjectAnnotation;
 import org.pnml.tools.epnk.applications.Application;
+import org.pnml.tools.epnk.applications.IApplicationWithPresentation;
 import org.pnml.tools.epnk.applications.hlpng.operations.AbstractOperator;
 import org.pnml.tools.epnk.applications.hlpng.operations.AddOperator;
 import org.pnml.tools.epnk.applications.hlpng.operations.DefaultOperator;
@@ -15,6 +18,7 @@ import org.pnml.tools.epnk.applications.hlpng.operations.NumberConstantOperator;
 import org.pnml.tools.epnk.applications.hlpng.operations.NumberOfOperator;
 import org.pnml.tools.epnk.applications.hlpng.operations.TupleOperator;
 import org.pnml.tools.epnk.helpers.FlatAccess;
+import org.pnml.tools.epnk.pnmlcoremodel.Arc;
 import org.pnml.tools.epnk.pnmlcoremodel.PetriNet;
 import org.pnml.tools.epnk.pnmlcoremodel.Transition;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Place;
@@ -25,23 +29,61 @@ import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.Term;
 import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.impl.TupleImpl;
 
 import runtime.AbstractValue;
+import runtime.MSValue;
 import runtime.NetMarking;
+import runtime.PlaceMarking;
 import runtime.RuntimeFactory;
 
-public class HLSimulator extends Application
+public class HLSimulator extends Application implements IApplicationWithPresentation
 {
-	private NetMarking netMarking = null;
+	protected NetMarking netMarking = null;
+	protected PetriNet petrinet = null;
+	protected IPresentationManager presentationManager = null;
+	protected Map<Place, MSValue> runtimeValues = null;
+	private Action[] actions;
 	
+	public IPresentationManager getPresentationManager()
+    {
+    	return presentationManager;
+    }
+
+	public void setPresentationManager(IPresentationManager presentationManager)
+    {
+    	this.presentationManager = presentationManager;
+    }
+
 	public HLSimulator(PetriNet petrinet)
     {
 	    super(petrinet);
 	    
+	    this.petrinet = petrinet;
+	    
 	    this.netMarking = evaluate(petrinet);
+	    
+	    this.runtimeValues = new HashMap<Place, MSValue>();
+	    for(PlaceMarking marking : netMarking.getMarkings())
+    	{
+    		runtimeValues.put(marking.getPlace(), marking.getMsValue());
+    	}
+	    
+	    this.presentationManager = new SimulatorPresentationManager(runtimeValues,
+	    		netMarking);
     }
+
+	public void auto()
+	{
+		System.out.println(HLSimulator.class + ": auto");
+	}
+	
+	public void stop()
+	{
+		System.out.println(HLSimulator.class + ": stop");
+	}
 	
 	@Override
 	public void initializeContents()
 	{
+
 		NetAnnotations netAnnotations = this.getNetAnnotations();
 		PetriNet petrinet = this.getPetrinet();
 
@@ -55,6 +97,32 @@ public class HLSimulator extends Application
 			objectAnnotation.setObject(transition);
 			netAnnotation.getObjectAnnotations().add(objectAnnotation);
 
+			for(Arc arc : transition.getIn())
+			{
+				objectAnnotation = NetannotationsFactory.eINSTANCE
+				        .createObjectAnnotation();
+				objectAnnotation.setObject(arc);
+				netAnnotation.getObjectAnnotations().add(objectAnnotation);
+
+				objectAnnotation = NetannotationsFactory.eINSTANCE
+				        .createObjectAnnotation();
+				objectAnnotation.setObject(arc.getSource());
+				netAnnotation.getObjectAnnotations().add(objectAnnotation);
+			}
+
+			for(Arc arc : transition.getOut())
+			{
+				objectAnnotation = NetannotationsFactory.eINSTANCE
+				        .createObjectAnnotation();
+				objectAnnotation.setObject(arc);
+				netAnnotation.getObjectAnnotations().add(objectAnnotation);
+
+				objectAnnotation = NetannotationsFactory.eINSTANCE
+				        .createObjectAnnotation();
+				objectAnnotation.setObject(arc.getTarget());
+				netAnnotation.getObjectAnnotations().add(objectAnnotation);
+			}
+
 			netAnnotations.getNetAnnotations().add(netAnnotation);
 		}
 
@@ -63,6 +131,7 @@ public class HLSimulator extends Application
 			netAnnotations
 			        .setCurrent(netAnnotations.getNetAnnotations().get(0));
 		}
+
 	}
 	
 	private static NetMarking evaluate(PetriNet petrinet)
@@ -96,15 +165,84 @@ public class HLSimulator extends Application
 				if(handler != null)
 				{
 					AbstractValue value = handlers.get(term.getClass()).handle(term);
-					System.out.println(value.toString());
-				}
-				else
-				{
-					System.out.println(term.getClass());
-					System.out.println("Cannot handle term: " + term);
+					
+					if(value instanceof MSValue)
+					{
+						PlaceMarking marking  = RuntimeFactory.eINSTANCE.createPlaceMarking();
+						marking.setPlace(hlPlace);
+						marking.setMsValue((MSValue)value);
+						
+						netMarking.getMarkings().add(marking);
+					}
+					else
+					{
+						System.err.println("ERR: " + HLSimulator.class + ": returned " +
+								"value is not an instance of " + MSValue.class);
+					}
 				}
 			}
 		}
 		return netMarking;
 	}
+	
+	@Override
+	public String getName() 
+	{
+		return "HLSimulator";
+	}
+	
+	@Override
+    public Action[] getActions()
+    {
+		if(actions == null)
+		{
+	    	actions = new Action[4];
+			
+			actions[0] = new Action() {
+				public void run() {
+					previousAnnotation();
+				}
+			};
+			actions[0].setId("previous");
+			actions[0].setText("Previous");
+			actions[0].setToolTipText("Show previous");
+//			actions[0].setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+//				getImageDescriptor("/home/mindaugas/Dropbox/DTU/master-project/workspace/org.pnml.tools.epnk.applications.hlpng.simulator/resources/back.png"));//ISharedImages.IMG_TOOL_BACK));
+			
+			actions[1] = new Action() {
+				public void run() {
+					auto();
+				}
+			};
+			actions[1].setId("auto");
+			actions[1].setText("Run");
+			actions[1].setToolTipText("Automatic mode");
+//			actions[1].setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+//				getImageDescriptor(ISharedImages.IMG_TOOL_UP));
+			
+			actions[2] = new Action() {
+				public void run() {
+					nextAnnotation();
+				}
+			};
+			actions[2].setId("stop");
+			actions[2].setText("Stop");
+			actions[2].setToolTipText("Stop current execution");
+//			actions[2].setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+//				getImageDescriptor(ISharedImages.IMG_TOOL_FORWARD));
+			
+			
+			actions[3] = new Action() {
+				public void run() {
+					stop();
+				}
+			};
+			actions[3].setId("next");
+			actions[3].setText("Next");
+			actions[3].setToolTipText("Show next");
+//			actions[3].setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+//				getImageDescriptor(ISharedImages.IMG_TOOL_FORWARD));
+	    }
+	    return actions;
+    }
 }
---
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/SimulatorPresentationManager.java
@@ -0,0 +1,82 @@
+package org.pnml.tools.epnk.applications.hlpng.simulator;
+
+import java.util.Map;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.Label;
+import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.widgets.Display;
+import org.pnml.tools.epnk.annotations.manager.IPresentationManager;
+import org.pnml.tools.epnk.annotations.netannotations.ObjectAnnotation;
+import org.pnml.tools.epnk.applications.hlpng.actions.SelectionHandler;
+import org.pnml.tools.epnk.applications.hlpng.view.LabelLayer;
+import org.pnml.tools.epnk.applications.hlpng.view.RectangleOverlay;
+import org.pnml.tools.epnk.applications.hlpng.view.TopRightLabel;
+import org.pnml.tools.epnk.pnmlcoremodel.Object;
+import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Place;
+import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Transition;
+
+import runtime.MSValue;
+import runtime.NetMarking;
+
+public class SimulatorPresentationManager implements IPresentationManager
+{
+	protected Map<Place, MSValue> runtimeValues = null;
+	protected NetMarking netMarking = null;
+	
+	protected SelectionHandler selectionHandler = null;
+	
+	public SimulatorPresentationManager(Map<Place, MSValue> runtimeValues,
+			NetMarking netMarking)
+	{
+		this.runtimeValues = runtimeValues;
+		this.netMarking = netMarking;
+		
+		this.selectionHandler = new SelectionHandler();
+	}
+	
+	public IFigure handle(Object owner, ObjectAnnotation objectAnnotation, 
+			AbstractGraphicalEditPart graphicalEditPart)
+	{
+		IFigure figure = null;
+		if(owner instanceof Transition)
+		{			
+			Transition transition = (Transition)owner;
+
+			IFigure coloredMarking = new RectangleOverlay(graphicalEditPart.getFigure(),
+					transition, runtimeValues);
+			coloredMarking.addMouseListener(selectionHandler);
+
+			return coloredMarking;
+		}
+		else if(owner instanceof Place)
+		{
+			Place place = (Place) owner;
+			
+			StringBuffer names = new StringBuffer();
+			if(place.getName() != null)
+			{
+				names.append(place.getName().getText() + "\n");
+			}
+			
+			IFigure mainFigure = graphicalEditPart.getFigure();
+			Font font = new Font(Display.getCurrent(), "Courier New", 8, SWT.NORMAL);
+			Label label = new TopRightLabel(font, currentMarking(runtimeValues, place), mainFigure);
+			return new LabelLayer(mainFigure, label);
+		}
+		return figure;
+	}
+	
+	private static String currentMarking(Map<Place, MSValue> runtimeValues, Place place)
+	{
+		MSValue value = runtimeValues.get(place);
+		
+		return value.toString();
+	}
+}
+
+
+
+
