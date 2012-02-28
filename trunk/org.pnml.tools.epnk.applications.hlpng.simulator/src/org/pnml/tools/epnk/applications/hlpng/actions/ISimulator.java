018c4eba532ae8d103f59379a170afd388e1ff50
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-02-28 08:58:09 +0000

    Simulator: extracted an interface for HLSimulator.java - ISimulator.java. SimulatorPresentationManager.java

---
+++ trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/actions/ISimulator.java
@@ -0,0 +1,9 @@
+package org.pnml.tools.epnk.applications.hlpng.actions;
+
+public interface ISimulator
+{
+	public void auto();
+	public void stop();
+	public void next();
+	public void previous();
+}
