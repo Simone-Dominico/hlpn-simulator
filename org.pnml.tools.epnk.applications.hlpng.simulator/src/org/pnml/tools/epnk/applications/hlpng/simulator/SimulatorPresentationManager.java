273d86ca62691aeca5d6cea3cbe0108a286b45c1
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-03-03 14:14:14 +0000

    Simulator: SimulatorPresentationManager.java recognizes transition marking now.

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/SimulatorPresentationManager.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/SimulatorPresentationManager.java
@@ -1,7 +1,5 @@
 package org.pnml.tools.epnk.applications.hlpng.simulator;
 
-import java.util.Map;
-
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
@@ -15,48 +13,43 @@ import org.pnml.tools.epnk.applications.hlpng.actions.SelectionHandler;
 import org.pnml.tools.epnk.applications.hlpng.view.LabelLayer;
 import org.pnml.tools.epnk.applications.hlpng.view.RectangleOverlay;
 import org.pnml.tools.epnk.applications.hlpng.view.TopRightLabel;
-import org.pnml.tools.epnk.pnmlcoremodel.Object;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Place;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Transition;
 
-import runtime.MSValue;
-import runtime.NetMarking;
+import runtime.PlaceMarking;
+import transitionruntime.TransitionMarking;
 
 public class SimulatorPresentationManager implements IPresentationManager
 {
-	protected Map<Place, MSValue> runtimeValues = null;
-	protected NetMarking netMarking = null;
-	
 	protected SelectionHandler selectionHandler = null;
 	protected ISimulator simulator = null;
 	
-	public SimulatorPresentationManager(Map<Place, MSValue> runtimeValues,
-			NetMarking netMarking, ISimulator simulator)
+	public SimulatorPresentationManager(ISimulator simulator)
 	{
-		this.runtimeValues = runtimeValues;
-		this.netMarking = netMarking;
 		this.simulator = simulator;
 		
 		this.selectionHandler = new SelectionHandler();
 	}
 	
-	public IFigure handle(Object owner, ObjectAnnotation objectAnnotation, 
+	public IFigure handle(ObjectAnnotation objectAnnotation, 
 			AbstractGraphicalEditPart graphicalEditPart)
 	{
 		IFigure figure = null;
-		if(owner instanceof Transition)
+		if(objectAnnotation instanceof TransitionMarking)
 		{			
-			Transition transition = (Transition)owner;
+			TransitionMarking marking = (TransitionMarking) objectAnnotation;
+			Transition transition = marking.getTransition();
 
 			IFigure coloredMarking = new RectangleOverlay(simulator,
-					graphicalEditPart.getFigure(), transition, runtimeValues);
+					graphicalEditPart.getFigure(), transition, marking);
 			coloredMarking.addMouseListener(selectionHandler);
 
 			return coloredMarking;
 		}
-		else if(owner instanceof Place)
+		else if(objectAnnotation instanceof PlaceMarking)
 		{
-			Place place = (Place) owner;
+			PlaceMarking marking = (PlaceMarking) objectAnnotation;
+			Place place = marking.getPlace();
 			
 			StringBuffer names = new StringBuffer();
 			if(place.getName() != null)
@@ -66,17 +59,10 @@ public class SimulatorPresentationManager implements IPresentationManager
 			
 			IFigure mainFigure = graphicalEditPart.getFigure();
 			Font font = new Font(Display.getCurrent(), "Courier New", 8, SWT.NORMAL);
-			Label label = new TopRightLabel(font, currentMarking(runtimeValues, place), mainFigure);
+			Label label = new TopRightLabel(font, marking.getMsValue().toString(), mainFigure);
 			return new LabelLayer(mainFigure, label);
 		}
 		return figure;
-	}
-	
-	private static String currentMarking(Map<Place, MSValue> runtimeValues, Place place)
-	{
-		MSValue value = runtimeValues.get(place);
-		
-		return value.toString();
 	}
 }
 
