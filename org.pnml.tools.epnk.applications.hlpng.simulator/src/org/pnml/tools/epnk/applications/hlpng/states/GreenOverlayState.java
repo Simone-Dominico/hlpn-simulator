cfad5e65a2cbe1467b03276609633be7a5bb8f45
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-03-06 07:41:52 +0000

    Simulator: a new package states/: BlueOverlayState.java GreenOverlayState.java IState.java IStateContext.java

---
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/states/GreenOverlayState.java
@@ -0,0 +1,23 @@
+package org.pnml.tools.epnk.applications.hlpng.states;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.pnml.tools.epnk.applications.hlpng.presentation.RectangleOverlay;
+
+public class GreenOverlayState implements IState
+{
+	protected RectangleOverlay overlay = null;
+	
+	public GreenOverlayState(RectangleOverlay overlay)
+	{
+		this.overlay = overlay;
+	}
+	
+	@Override
+	public void handle()
+	{
+		overlay.setForegroundColor(ColorConstants.green);
+		overlay.setBackgroundColor(ColorConstants.green);
+		overlay.setState(new BlueOverlayState(overlay));
+	}
+
+}
---
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/states/IState.java
@@ -0,0 +1,6 @@
+package org.pnml.tools.epnk.applications.hlpng.states;
+
+public interface IState
+{
+	public void handle();
+}
