abd7ee204bac06009bca48429574f5158f270d2d
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-03-12 08:51:41 +0000

    Simulator: BUG: when more than 1 variable is in an arc inscription. ArcInscriptionManager.java SructuralPatternMatcher.java TransitionManager.java InscriptionMatch.java VariableAssignmnet.java

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/firing/TransitionManager.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/firing/TransitionManager.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.pnml.tools.epnk.applications.hlpng.utils.CartesianProduct;
 import org.pnml.tools.epnk.applications.hlpng.utils.Pair;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Arc;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Place;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Transition;
 
-import runtime.AbstractValue;
 import runtime.PlaceMarking;
 import transitionruntime.FiringData;
 import transitionruntime.FiringMode;
 import transitionruntime.MSTerm;
 import transitionruntime.TransitionMarking;
 import transitionruntime.TransitionruntimeFactory;
+import runtime.RuntimeVariable;
 
 public class TransitionManager
 {
@@ -29,156 +30,138 @@ public class TransitionManager
 	public TransitionMarking checkTransition(Transition transition, Map<String, 
 			PlaceMarking> runtimeValues)
 	{
-		List<Pair<String, List<Pair<AbstractValue, Integer>>>> entries = 
-				new ArrayList<Pair<String, List<Pair<AbstractValue, Integer>>>>();
-		
+		// for each term in an inscription find all possible assignments
+		List<List<InscriptionMatch>> entries = new ArrayList<List<InscriptionMatch>>();
+
 		for(org.pnml.tools.epnk.pnmlcoremodel.Arc arc : transition.getIn())
 		{
 			Place place = (Place) arc.getSource();
 			PlaceMarking placeMarking = runtimeValues.get(place.getId());
 			
-			List<Pair<AbstractValue, Integer>> matches = arcInscriptionManager
+			List<InscriptionMatch> matches = arcInscriptionManager
 					.matchesInscription((Arc)arc, placeMarking, placeMarking.isDirty());
 			
 			if(matches != null)
 			{
-				entries.add(new Pair<String, List<Pair<AbstractValue, Integer>>>(place.getId(),
-						matches));
+				entries.add(matches);
 			}
 		}
+		
+		if(entries.size() > 0 && entries.size() == transition.getIn().size())
+		{	
+			List<List<Pair<InscriptionMatch, VariableAssignmnet>>> product = null;
+			
+			if(entries.size() > 1)
+			{
+				List<List<Pair<InscriptionMatch, VariableAssignmnet>>> subsets =
+						new ArrayList<List<Pair<InscriptionMatch,VariableAssignmnet>>>();
+				for(List<InscriptionMatch> entry : entries)
+				{
+					List<Pair<InscriptionMatch, VariableAssignmnet>> matchPairs =
+							new ArrayList<Pair<InscriptionMatch,VariableAssignmnet>>();
+					for(InscriptionMatch match : entry)
+					{
+						for(VariableAssignmnet assignmnet : match.getAssignmnets())
+						{
+							Pair<InscriptionMatch, VariableAssignmnet> pair = 
+									new Pair<InscriptionMatch, VariableAssignmnet>(match, assignmnet);
+							matchPairs.add(pair);
+						}
+					}
+					subsets.add(matchPairs);
+				}
+				
+				CartesianProduct<Pair<InscriptionMatch, VariableAssignmnet>> cartesianProduct = 
+						new CartesianProduct<Pair<InscriptionMatch, VariableAssignmnet>>();
+				
+				product = cartesianProduct.product(subsets);
+			}
+			
+			TransitionMarking marking = TransitionruntimeFactory.eINSTANCE.createTransitionMarking();
 
-		if(entries.size() == transition.getIn().size())
-		{
-			return cartesianProduct(entries, runtimeValues);
+			if(product != null)
+			{
+				for(List<Pair<InscriptionMatch, VariableAssignmnet>> subset : product)
+				{
+					marking.getModes().add(createFiringMode(subset, runtimeValues));
+				}
+			}
+			else
+			{
+				for(InscriptionMatch match : entries.get(0))
+				{
+					List<FiringMode> list = createFiringMode(match, runtimeValues);
+					for(FiringMode mode : list)
+					{
+						marking.getModes().add(mode);
+					}
+				}
+			}
+			return marking;
 		}
 		
 		return null;
 	}
 	
-	private static TransitionMarking cartesianProduct(
-			List<Pair<String, List<Pair<AbstractValue, Integer>>>> modes,
+	private static FiringMode createFiringMode(
+			List<Pair<InscriptionMatch, VariableAssignmnet>> matches,
 			Map<String, PlaceMarking> runtimeValues)
 	{
-		TransitionMarking marking = TransitionruntimeFactory.eINSTANCE.createTransitionMarking();
+		FiringMode mode = TransitionruntimeFactory.eINSTANCE.createFiringMode();
 		
-		if(modes.size() == 1)
+		for(Pair<InscriptionMatch, VariableAssignmnet> matchPair : matches)
 		{
-			for(Pair<String, List<Pair<AbstractValue, Integer>>> pair : modes)
-			{			
-				String placeId = pair.getKey();
-				
-				for(Pair<AbstractValue, Integer> value : pair.getValue())
-				{
-					MSTerm msTerm = TransitionruntimeFactory.eINSTANCE.createMSTerm();
-					msTerm.setMultiplicity(value.getValue());
-					msTerm.setPlaceId(placeId);
-					msTerm.setValue(value.getKey());
+			InscriptionMatch match = matchPair.getKey();
+			VariableAssignmnet assignmnet = matchPair.getValue();
+			
+			MSTerm msTerm = TransitionruntimeFactory.eINSTANCE.createMSTerm();
+			msTerm.setMultiplicity(match.getMultiplicity());
+			msTerm.setPlaceId(match.getPlaceId());
+			msTerm.setValue(assignmnet.getParentValue());
+			
+			for(RuntimeVariable variable : assignmnet.getAssignments().keySet())
+			{
+				FiringData data = TransitionruntimeFactory.eINSTANCE.createFiringData();
+				data.setPlaceMarking(runtimeValues.get(match.getPlaceId()));
+				data.setMsTerm(msTerm);
+				data.setVariable(variable);
+				data.setVarValue(assignmnet.getAssignments().get(variable));
 
-					FiringData data = TransitionruntimeFactory.eINSTANCE.createFiringData();
-					data.setPlaceMarking(runtimeValues.get(placeId));
-					data.setMsTerm(msTerm);
-					
-					FiringMode mode = TransitionruntimeFactory.eINSTANCE.createFiringMode();
-					mode.getValues().add(data);
-					marking.getModes().add(mode);
-				}
+				mode.getValues().add(data);	
 			}
 		}
-		else if(modes.size() > 1)
-		{
-			List<List<MSTerm>> partialModes = cartesianProduct(modes.get(0), modes.get(1));
-			
-			for(int i = 2; i < modes.size(); i++)
-			{
-				partialModes = cartesianProduct(partialModes, modes.get(i));
-			}
-			
-			for(List<MSTerm> partialMode : partialModes)
-			{
-				FiringMode mode = TransitionruntimeFactory.eINSTANCE.createFiringMode();
-				
-				for(MSTerm term : partialMode)
-				{
-					MSTerm msTerm = TransitionruntimeFactory.eINSTANCE.createMSTerm();
-					msTerm.setMultiplicity(term.getMultiplicity());
-					msTerm.setPlaceId(term.getPlaceId());
-					msTerm.setValue(term.getValue());
-					
-					FiringData data = TransitionruntimeFactory.eINSTANCE.createFiringData();
-					data.setPlaceMarking(runtimeValues.get(term.getPlaceId()));
-					data.setMsTerm(msTerm);
 		
-					mode.getValues().add(data);
-				}
-				
-				if(mode.getValues().size() > 0)
-				{
-					marking.getModes().add(mode);
-				}
-			}
-		}
-		return marking;
+		return mode;
 	}
 	
-	private static List<List<MSTerm>> cartesianProduct(
-			Pair<String, List<Pair<AbstractValue, Integer>>> p1,
-			Pair<String, List<Pair<AbstractValue, Integer>>> p2)
+	private static List<FiringMode> createFiringMode(InscriptionMatch match,
+			Map<String, PlaceMarking> runtimeValues)
 	{
-		List<List<MSTerm>> result = new ArrayList<List<MSTerm>>();
+		List<FiringMode> list = new ArrayList<FiringMode>();
 		
-		for(Pair<AbstractValue, Integer> a1 : p1.getValue())
-		{
-			for(Pair<AbstractValue, Integer> a2 : p2.getValue())
+		for(VariableAssignmnet assignmnet : match.getAssignmnets())
+		{	
+			FiringMode mode = TransitionruntimeFactory.eINSTANCE.createFiringMode();
+			
+			MSTerm msTerm = TransitionruntimeFactory.eINSTANCE.createMSTerm();
+			msTerm.setMultiplicity(match.getMultiplicity());
+			msTerm.setPlaceId(match.getPlaceId());
+			msTerm.setValue(assignmnet.getParentValue());
+			
+			for(RuntimeVariable variable : assignmnet.getAssignments().keySet())
 			{
-				List<MSTerm> l = new ArrayList<MSTerm>();
-				
-				{
-					MSTerm term = TransitionruntimeFactory.eINSTANCE.createMSTerm();
-					term.setMultiplicity(a1.getValue());
-					term.setPlaceId(p1.getKey());
-					term.setValue(a1.getKey());
-					
-					l.add(term);
-				}
-				{
-					MSTerm term = TransitionruntimeFactory.eINSTANCE.createMSTerm();
-					term.setMultiplicity(a2.getValue());
-					term.setPlaceId(p2.getKey());
-					term.setValue(a2.getKey());
-					
-					l.add(term);
-				}
-				
-				result.add(l);
+				FiringData data = TransitionruntimeFactory.eINSTANCE.createFiringData();
+				data.setPlaceMarking(runtimeValues.get(match.getPlaceId()));
+				data.setMsTerm(msTerm);
+				data.setVariable(variable);
+				data.setVarValue(assignmnet.getAssignments().get(variable));
+
+				mode.getValues().add(data);	
 			}
+			
+			list.add(mode);
 		}
-		return result;
-	}
-	
-	private static List<List<MSTerm>> cartesianProduct(
-			List<List<MSTerm>> oldResult, 
-			Pair<String, List<Pair<AbstractValue, Integer>>> p)
-	{
-		List<List<MSTerm>> result = new ArrayList<List<MSTerm>>();
 		
-		for(List<MSTerm> subset : oldResult)
-		{
-			List<MSTerm> l = new ArrayList<MSTerm>();
-			for(MSTerm p1 : subset)
-			{
-				l.add(p1);
-				for(Pair<AbstractValue, Integer> a : p.getValue())
-				{
-					MSTerm term = TransitionruntimeFactory.eINSTANCE.createMSTerm();
-					term.setMultiplicity(a.getValue());
-					term.setPlaceId(p.getKey());
-					term.setValue(a.getKey());
-					
-					l.add(term);
-				}
-			}
-			result.add(l);
-		}
-		return result;
+		return list;
 	}
 }
