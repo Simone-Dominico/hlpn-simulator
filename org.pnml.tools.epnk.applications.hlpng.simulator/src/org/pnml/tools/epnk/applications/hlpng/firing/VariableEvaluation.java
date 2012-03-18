977eb765631b79534c657008f618efbbb4ed5360
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-03-18 16:34:01 +0000

    VariableAssignmnet.java replace with VariableEvaluation.java.

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/firing/VariableEvaluation.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/firing/VariableEvaluation.java
@@ -1,15 +1,23 @@
 package org.pnml.tools.epnk.applications.hlpng.firing;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.pnml.tools.epnk.applications.hlpng.runtime.AbstractValue;
+import org.pnml.tools.epnk.applications.hlpng.runtime.MSValue;
 import org.pnml.tools.epnk.applications.hlpng.runtime.RuntimeVariable;
 
 public class VariableEvaluation
 {
+	private MSValue multiset = null;
+	
 	private RuntimeVariable variable = null;
-	private List<AbstractValue> values = new ArrayList<AbstractValue>();
+	
+	// an actual assignment, the a set of multiset terms where the assignments belong to
+	private Map<AbstractValue, Set<AbstractValue>> assignmnets = 
+			new HashMap<AbstractValue, Set<AbstractValue>>();
 	
 	public RuntimeVariable getVariable()
     {
@@ -19,8 +27,41 @@ public class VariableEvaluation
     {
     	this.variable = variable;
     }
-	public List<AbstractValue> getValues()
+	
+	public MSValue getMultiset()
     {
-    	return values;
+    	return multiset;
+    }
+	public void setMultiset(MSValue multiset)
+    {
+    	this.multiset = multiset;
+    }
+	
+	public void registerAssignment(AbstractValue key)
+	{
+		registerAssignment(key, new HashSet<AbstractValue>());
+	}
+	public void registerAssignment(Map<AbstractValue, Set<AbstractValue>> values)
+	{
+		for(AbstractValue key : values.keySet())
+		{
+			registerAssignment(key, values.get(key));
+		}
+	}
+	public void registerAssignment(AbstractValue key, Set<AbstractValue> value)
+	{
+		if(assignmnets.containsKey(key))
+		{
+			Set<AbstractValue> values = assignmnets.get(key);
+			values.addAll(value);
+		}
+		else
+		{
+			assignmnets.put(key, value);
+		}
+	}
+	public Map<AbstractValue, Set<AbstractValue>> getAssignmnets()
+    {
+    	return assignmnets;
     }
 }
