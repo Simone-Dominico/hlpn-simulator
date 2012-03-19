cbfd65ad2ecc06d21d5d777a249110447fde7f31
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-03-19 10:53:45 +0000

    renamed operators/ to runtime.operators/.

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/runtime/operators/TermManager.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/runtime/operators/TermManager.java
@@ -1,18 +1,12 @@
-package org.pnml.tools.epnk.applications.hlpng.operators;
+package org.pnml.tools.epnk.applications.hlpng.runtime.operators;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.pnml.tools.epnk.applications.hlpng.runtime.AbstractBinaryOperation;
-import org.pnml.tools.epnk.applications.hlpng.runtime.RuntimeAddition;
-import org.pnml.tools.epnk.applications.hlpng.runtime.RuntimeMultiplication;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.integers.impl.AdditionImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.integers.impl.MultiplicationImpl;
 import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.integers.impl.NumberConstantImpl;
 import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.multisets.impl.AddImpl;
 import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.multisets.impl.NumberOfImpl;
 import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.impl.TupleImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.impl.VariableImpl;
 
 public class TermManager
 {
@@ -27,10 +21,6 @@ public class TermManager
 			handlers.put(NumberOfImpl.class, new NumberOfOperator(this, defaultOp));
 			handlers.put(AddImpl.class, new AddOperator(this, defaultOp));
 			handlers.put(TupleImpl.class, new TupleOperator(this, defaultOp));
-			handlers.put(VariableImpl.class, new VariableHandler(this, defaultOp));
-			
-			handlers.put(AdditionImpl.class, new BinaryIntegerOperator(this, defaultOp));
-			handlers.put(MultiplicationImpl.class, new BinaryIntegerOperator(this, defaultOp));
 		}
 	}
 	
@@ -47,19 +37,5 @@ public class TermManager
 	public AbstractTermHandler getHandler(Class targetClass)
 	{
 		return handlers.get(targetClass);
-	}
-	
-	public static AbstractBinaryOperation createOperation(Class name)
-	{
-		if(name.equals(AdditionImpl.class))
-		{
-			return new RuntimeAddition();
-		}
-		if(name.equals(MultiplicationImpl.class))
-		{
-			return new RuntimeMultiplication();
-		}
-		
-		throw new RuntimeException("Do not know the operation!");
 	}
 }
