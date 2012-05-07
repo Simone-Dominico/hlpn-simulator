074c77d385c6dbe1b8520ab76d244e0322d09026
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-05-07 07:13:48 +0000

    Removed a property "resolved" from TermWrapper.java

--- trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/firing/TermWrapper.java
+++ trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/firing/TermWrapper.java
@@ -7,7 +7,6 @@ import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.Term;
 public class TermWrapper extends AbstractValue
 {
 	protected Term rootTerm = null;
-	protected boolean resolved = true;
 
 	public Term getRootTerm()
     {
@@ -21,27 +20,15 @@ public class TermWrapper extends AbstractValue
     {
     	return rootTerm.toString();
     }
-
-	public boolean isResolved()
-    {
-    	return resolved;
-    }
-	public void setResolved(boolean resolved)
-    {
-    	this.resolved = resolved;
-    }
-	
 	@Override
     public int hashCode()
     {
 	    final int prime = 31;
 	    int result = 1;
-	    result = prime * result + (resolved ? 1231 : 1237);
 	    result = prime * result
 	            + ((rootTerm == null) ? 0 : rootTerm.hashCode());
 	    return result;
     }
-	
 	@Override
     public boolean equals(Object obj)
     {
@@ -58,10 +45,6 @@ public class TermWrapper extends AbstractValue
 		    return false;
 	    }
 	    TermWrapper other = (TermWrapper) obj;
-	    if(resolved != other.resolved)
-	    {
-		    return false;
-	    }
 	    if(rootTerm == null)
 	    {
 		    if(other.rootTerm != null)
