39f54271f8f6a746e9aa7cd233b911caaf3efc73
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-05-08 14:12:03 +0000

    NetworkSimulator: initial version of min-dist

--- trunk/org.pnml.tools.epnk.applications.hlpng.networksimulator/src/org/pnml/tools/epnk/applications/hlpng/utils/NodeWrapper.java
+++ trunk/org.pnml.tools.epnk.applications.hlpng.networksimulator/src/org/pnml/tools/epnk/applications/hlpng/utils/NodeWrapper.java
@@ -1,6 +1,25 @@
 package org.pnml.tools.epnk.applications.hlpng.utils;
 
+import networkmodel.Node;
+
 public class NodeWrapper
 {
+	private int id = 0;
+	private Node node = null;
+	
+	public NodeWrapper(int id, Node node)
+	{
+		this.id = id;
+		this.node = node;
+	}
 
+	public int getId()
+    {
+    	return id;
+    }
+
+	public Node getNode()
+    {
+    	return node;
+    }
 }
