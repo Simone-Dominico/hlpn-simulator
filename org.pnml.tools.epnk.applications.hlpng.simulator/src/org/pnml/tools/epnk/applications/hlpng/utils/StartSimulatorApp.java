195ea50d5be41565d6994536f9afeb911c0a7b21
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-05-11 13:29:43 +0000

    Simulator: resource management

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/utils/StartSimulatorApp.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/utils/StartSimulatorApp.java
@@ -1,8 +1,5 @@
 package org.pnml.tools.epnk.applications.hlpng.utils;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -12,39 +9,10 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.pnml.tools.epnk.applications.activator.Activator;
 import org.pnml.tools.epnk.applications.hlpng.simulator.HLSimulator;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.ComparisonManager;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.DatatypesComparator;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.MultisetComparator;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.NumberOfComparator;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.ReversibleOperationComparator;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.TupleComparator;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.UserOperatorComparator;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.VariableComparator;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.AdditionEval;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.BooleansEval;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.EvaluationManager;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.IEvaluator;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.IntegersEval;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.MultiplicationEval;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.MultisetsEval;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.ReversibleOperationManager;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.StringsEval;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.SubtractionEval;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.TermsEval;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.UserOperatorEval;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.VariableEval;
 import org.pnml.tools.epnk.applications.registry.ApplicationRegistry;
 import org.pnml.tools.epnk.pnmlcoremodel.PetriNet;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.booleans.impl.BooleanConstantImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.integers.impl.AdditionImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.integers.impl.MultiplicationImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.integers.impl.NumberConstantImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.integers.impl.SubtractionImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.multisets.impl.NumberOfImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.strings.impl.StringConstantImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.impl.MultiSetOperatorImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.impl.TupleImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.impl.UserOperatorImpl;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.impl.VariableImpl;
 
 public class StartSimulatorApp implements IObjectActionDelegate
 {
@@ -56,13 +24,13 @@ public class StartSimulatorApp implements IObjectActionDelegate
 	{
 		// init the evaluation manager
 		EvaluationManager evaluationManager = 
-				createEvaluationManager("org.pnml.tools.epnk.applications.hlpng.transitionBinding.extensions");
+				ResourceManager.createEvaluationManager("org.pnml.tools.epnk.applications.hlpng.transitionBinding.extensions");
 		
 		// init the reversible operation manager
-		ReversibleOperationManager reversibleOperationManager = createReversibleOperationManager(evaluationManager);
+		ReversibleOperationManager reversibleOperationManager = ResourceManager.createReversibleOperationManager(evaluationManager);
 				
 		// init the comparison manager
-		ComparisonManager comparisonManager = createComparisonManager(evaluationManager, reversibleOperationManager);
+		ComparisonManager comparisonManager = ResourceManager.createComparisonManager(evaluationManager, reversibleOperationManager);
 
 		// creates a simulator
 		HLSimulator application = new HLSimulator(petrinet, evaluationManager, 
@@ -74,83 +42,6 @@ public class StartSimulatorApp implements IObjectActionDelegate
 		registry.addApplication(application);
 	}
 	
-	private static EvaluationManager createEvaluationManager(String extensionId)
-	{
-		EvaluationManager evaluationManager = new EvaluationManager();
-		// integers package
-		evaluationManager.register(NumberConstantImpl.class.getPackage(), new IntegersEval());
-		// booleans package
-		evaluationManager.register(BooleanConstantImpl.class.getPackage(), new BooleansEval());
-		// strings package
-		evaluationManager.register(StringConstantImpl.class.getPackage(), new StringsEval());			
-		// multisets package
-		evaluationManager.register(NumberOfImpl.class.getPackage(), new MultisetsEval());
-		// terms package
-		evaluationManager.register(TupleImpl.class.getPackage(), new TermsEval());
-		// variables
-		evaluationManager.register(VariableImpl.class, new VariableEval());
-		// user operations
-		UserOperatorEval userOperatorEval = new UserOperatorEval();
-		evaluationManager.register(UserOperatorImpl.class, userOperatorEval);
-		
-		evaluationManager.register(AdditionImpl.class, new AdditionEval());
-		evaluationManager.register(MultiplicationImpl.class, new MultiplicationEval());
-		evaluationManager.register(SubtractionImpl.class, new SubtractionEval());
-		
-		// user extensions
-		IConfigurationElement[] config = Platform.getExtensionRegistry().getConfigurationElementsFor(extensionId);
-		for(IConfigurationElement e : config)
-		{
-			try
-			{
-				IEvaluator arbitraryOperatorEval = (IEvaluator) e.createExecutableExtension("class");
-				userOperatorEval.setArbitraryOperatorEvaluator(arbitraryOperatorEval);
-			}
-			catch(CoreException e1)
-			{
-				e1.printStackTrace();
-			}
-		}
-		return evaluationManager;
-	}
-	
-	private static ReversibleOperationManager createReversibleOperationManager(EvaluationManager evaluationManager)
-	{
-		ReversibleOperationManager reversibleOperationManager = new ReversibleOperationManager(evaluationManager);
-		
-		reversibleOperationManager.register(AdditionImpl.class, new AdditionEval());
-		reversibleOperationManager.register(MultiplicationImpl.class, new MultiplicationEval());
-		reversibleOperationManager.register(SubtractionImpl.class, new SubtractionEval());
-		
-		return reversibleOperationManager;
-	}
-	
-	private static ComparisonManager createComparisonManager(EvaluationManager evaluationManager,
-			ReversibleOperationManager reversibleOperationManager)
-	{
-		ComparisonManager comparisonManager = new ComparisonManager();
-		
-		DatatypesComparator datatypesComparator = new DatatypesComparator();
-		comparisonManager.register(NumberConstantImpl.class.getPackage(), datatypesComparator);
-		comparisonManager.register(BooleanConstantImpl.class.getPackage(), datatypesComparator);
-		comparisonManager.register(StringConstantImpl.class.getPackage(), datatypesComparator);
-		
-		comparisonManager.register(TupleImpl.class, new TupleComparator(comparisonManager));
-		comparisonManager.register(MultiSetOperatorImpl.class, new MultisetComparator(comparisonManager));
-		comparisonManager.register(NumberOfImpl.class, new NumberOfComparator(comparisonManager));
-		
-		ReversibleOperationComparator binEval = 
-				new ReversibleOperationComparator(evaluationManager, reversibleOperationManager);
-		comparisonManager.register(AdditionImpl.class, binEval);
-		comparisonManager.register(MultiplicationImpl.class, binEval);
-		comparisonManager.register(SubtractionImpl.class, binEval);
-		
-		comparisonManager.register(VariableImpl.class, new VariableComparator());
-		comparisonManager.register(UserOperatorImpl.class, new UserOperatorComparator());
-		
-		return comparisonManager;
-	}
-
 	@Override
 	public void selectionChanged(IAction action, ISelection selection)
 	{
