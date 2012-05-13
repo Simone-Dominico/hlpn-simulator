33b8f64c7588ffe9d4ba667fac9d84b0f2f8b8b4
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-05-13 07:36:10 +0000

    Updated Consensus example

--- org.pnml.tools.epnk.applications.hlpng.networksimulator/src/org/pnml/tools/epnk/applications/hlpng/network/consensus/MFunction.java
+++ org.pnml.tools.epnk.applications.hlpng.networksimulator/src/org/pnml/tools/epnk/applications/hlpng/network/consensus/MFunction.java
@@ -6,40 +6,81 @@ import java.util.Map;
 
 import org.pnml.tools.epnk.applications.hlpng.runtime.AbstractValue;
 import org.pnml.tools.epnk.applications.hlpng.runtime.MSValue;
+import org.pnml.tools.epnk.applications.hlpng.runtime.ProductValue;
+import org.pnml.tools.epnk.applications.hlpng.runtime.StringValue;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.TermWrapper;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.EvaluationManager;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.IEvaluator;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.UnknownVariableException;
+import org.pnml.tools.epnk.applications.hlpng.utils.NodeWrapper;
 import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.Operator;
 import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.Term;
+import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.TermsFactory;
 import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.UserOperator;
 
 public class MFunction implements IEvaluator
 {
-	@Override
-	public AbstractValue evaluate(Term term, EvaluationManager evaluationManager,
-			Map<TermWrapper, AbstractValue> assignments) throws UnknownVariableException
-	{
-		Operator operator = (Operator) term;
-		List<AbstractValue> values = new ArrayList<AbstractValue>();
-		for(Term subterm : operator.getSubterm())
-		{
-			IEvaluator evaluator = evaluationManager.getHandler(subterm.getClass()); 
-			AbstractValue value = evaluator.evaluate(subterm, evaluationManager, assignments);
-			values.add(value);
-		}
-			
-		UserOperator uOp = (UserOperator) operator;
-		
-		MSValue msValue = new MSValue();
-		msValue.setSort(uOp.getOutputSort());
-	    
-		for(AbstractValue value : Database.getMessages())
-		{
-			msValue.getValues().put(value, 1);
-		}
-		
-	    return msValue;
+    private List<AbstractValue> messages = null;
+    private List<NodeWrapper> nodes = null;
+    
+    public MFunction(List<NodeWrapper> nodes)
+    {
+        this.nodes = nodes;
+    }
+    
+    @Override
+    public AbstractValue evaluate(Term term, EvaluationManager evaluationManager,
+            Map<TermWrapper, AbstractValue> assignments) throws UnknownVariableException
+    {
+        Operator operator = (Operator) term;
+        List<AbstractValue> values = new ArrayList<AbstractValue>();
+        for(Term subterm : operator.getSubterm())
+        {
+            IEvaluator evaluator = evaluationManager.getHandler(subterm.getClass()); 
+            AbstractValue value = evaluator.evaluate(subterm, evaluationManager, assignments);
+            values.add(value);
+        }
+            
+        UserOperator uOp = (UserOperator) operator;
+        
+        MSValue msValue = new MSValue();
+        msValue.setSort(uOp.getOutputSort());
+        
+        for(AbstractValue value : getMessages())
+        {
+            msValue.getValues().put(value, 1);
+        }
+        
+        return msValue;
+    }
+    
+    public List<AbstractValue> getMessages()
+    {
+        if(messages == null)
+        {
+            messages = new ArrayList<AbstractValue>();
+            for(NodeWrapper i : nodes)
+            {
+                for(NodeWrapper j : nodes)
+                {
+                    StringValue strVal0 = new StringValue();
+                    strVal0.setData(i.getNode().getLabel());
+                    strVal0.setSort(TermsFactory.eINSTANCE.createUserSort());
+                    
+                    StringValue strVal1 = new StringValue();
+                    strVal1.setData(j.getNode().getLabel());
+                    strVal1.setSort(TermsFactory.eINSTANCE.createUserSort());
+                    
+                    ProductValue pValue = new ProductValue();
+                    pValue.setSort(TermsFactory.eINSTANCE.createProductSort());
+                    pValue.getComponents().add(strVal0);
+                    pValue.getComponents().add(strVal1);
+                    
+                    messages.add(pValue);   
+                }
+            }   
+        }
+        return messages;
     }
 
 }
--- org.pnml.tools.epnk.applications.hlpng.networksimulator/src/org/pnml/tools/epnk/applications/hlpng/network/consensus/RFFunction.java
+++ org.pnml.tools.epnk.applications.hlpng.networksimulator/src/org/pnml/tools/epnk/applications/hlpng/network/consensus/RFFunction.java
@@ -1,12 +1,13 @@
 package org.pnml.tools.epnk.applications.hlpng.network.consensus;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import org.pnml.tools.epnk.applications.hlpng.runtime.AbstractValue;
 import org.pnml.tools.epnk.applications.hlpng.runtime.MSValue;
+import org.pnml.tools.epnk.applications.hlpng.runtime.ProductValue;
+import org.pnml.tools.epnk.applications.hlpng.runtime.StringValue;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.TermWrapper;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.EvaluationManager;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.IEvaluator;
@@ -17,30 +18,52 @@ import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.UserOperator;
 
 public class RFFunction implements IEvaluator
 {
-	@Override
-	public AbstractValue evaluate(Term term, EvaluationManager evaluationManager,
-			Map<TermWrapper, AbstractValue> assignments) throws UnknownVariableException
-	{
-		Operator operator = (Operator) term;
-		List<AbstractValue> values = new ArrayList<AbstractValue>();
-		for(Term subterm : operator.getSubterm())
-		{
-			IEvaluator evaluator = evaluationManager.getHandler(subterm.getClass()); 
-			AbstractValue value = evaluator.evaluate(subterm, evaluationManager, assignments);
-			values.add(value);
-		}
-			
-		UserOperator uOp = (UserOperator) operator;
-		
-		MSValue msValue = new MSValue();
-		msValue.setSort(uOp.getOutputSort());
-	    
-		List<AbstractValue> list = new ArrayList<AbstractValue>(values);
-		for(AbstractValue value : Database.getMessagesBySender(list.get(0)))
-		{
-			msValue.getValues().put(value, 1);
-		}
-		
-	    return msValue;
+    private List<AbstractValue> messages = null;
+    
+    public RFFunction(List<AbstractValue> messages)
+    {
+        this.messages = messages;
+    }
+    
+    @Override
+    public AbstractValue evaluate(Term term, EvaluationManager evaluationManager,
+            Map<TermWrapper, AbstractValue> assignments) throws UnknownVariableException
+    {
+        Operator operator = (Operator) term;
+        List<AbstractValue> values = new ArrayList<AbstractValue>();
+        for(Term subterm : operator.getSubterm())
+        {
+            IEvaluator evaluator = evaluationManager.getHandler(subterm.getClass()); 
+            AbstractValue value = evaluator.evaluate(subterm, evaluationManager, assignments);
+            values.add(value);
+        }
+            
+        UserOperator uOp = (UserOperator) operator;
+        
+        MSValue msValue = new MSValue();
+        msValue.setSort(uOp.getOutputSort());
+        
+        List<AbstractValue> list = new ArrayList<AbstractValue>(values);
+        for(AbstractValue value : getMessagesBySender(list.get(0), messages))
+        {
+            msValue.getValues().put(value, 1);
+        }
+        
+        return msValue;
+    }
+    
+    private static List<AbstractValue> getMessagesBySender(AbstractValue sender, List<AbstractValue> allMessages)
+    {
+        List<AbstractValue> messages = new ArrayList<AbstractValue>();
+        
+        for(AbstractValue pValue : allMessages)
+        {
+            StringValue s = (StringValue)((ProductValue)pValue).getComponents().get(0);
+            if(s.getData().equals(((StringValue)sender).getData()))
+            {
+                messages.add(pValue);
+            }
+        }
+        return messages;
     }
 }
