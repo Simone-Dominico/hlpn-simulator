f124015868a840dbcbc4be9fd4b502138d3d5cce
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-05-15 07:51:54 +0000

    Improving TransitionManager.java & VariableResolver.java

--- trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/firing/VariableResolver.java
+++ trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/firing/VariableResolver.java
@@ -1,44 +1,104 @@
 package org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.eclipse.swt.widgets.Display;
+import org.pnml.tools.epnk.applications.hlpng.presentation.InputDialog;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.AbstractReversibleOperation;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.ReversibleOperationManager;
-import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.Variable;
+import org.pnml.tools.epnk.applications.hlpng.utils.Pair;
 
 public class VariableResolver
 {
 	private Map<TermWrapper, TermAssignment> termMap = null;
 	private ReversibleOperationManager reversibleOperationManager = null;
+	private List<Pair<Set<TermWrapper>, Map<TermWrapper, TermAssignment>>> varsAndMatches = null;
 	
-	public VariableResolver(Map<TermWrapper, TermAssignment> termMap,
+	private List<TermAssignment> termAssigments = null;
+	private List<TermWrapper> resolved = null;
+	
+	public VariableResolver(List<Pair<Set<TermWrapper>, Map<TermWrapper, TermAssignment>>> varsAndMatches,
+			Map<TermWrapper, TermAssignment> termMap,
 			ReversibleOperationManager reversibleOperationManager)
 	{
+		this.varsAndMatches = varsAndMatches;
 		this.termMap = termMap;
 		this.reversibleOperationManager = reversibleOperationManager;
+		
+		// resolved vars and unresolved terms
+		this.termAssigments = new ArrayList<TermAssignment>();
+		this.resolved = new ArrayList<TermWrapper>();
+		for(TermWrapper key : termMap.keySet())
+		{
+			TermAssignment ve = termMap.get(key);
+			if(ve.getTermWrapper() instanceof VariableWrapper)
+			{
+				resolved.add(ve.getTermWrapper());
+			}
+			else
+			{
+				termAssigments.add(ve);
+			}
+		}
+		
+		// remove all term assignments
+		for(TermAssignment ve : termAssigments)
+		{
+			termMap.remove(ve.getTermWrapper());
+		}
+		
+		// remove all resolved variables
+		for(Pair<Set<TermWrapper>, Map<TermWrapper, TermAssignment>> p : varsAndMatches)
+		{
+			p.getKey().removeAll(resolved);
+		}
+		
+		// sort by the number of unresolved variables
+		Collections.sort(varsAndMatches, new Comparator<Pair<Set<TermWrapper>, Map<TermWrapper, TermAssignment>>>()
+		{
+			@Override
+            public int compare(
+                    Pair<Set<TermWrapper>, Map<TermWrapper, TermAssignment>> o1,
+                    Pair<Set<TermWrapper>, Map<TermWrapper, TermAssignment>> o2)
+            {
+	            return o1.getKey().size() - o2.getKey().size();
+            }
+		});
+		
+		// remove arcs which assignments are completely resolved
+		while(varsAndMatches.size() > 0 && varsAndMatches.get(0).getKey().size() == 0)
+		{
+			varsAndMatches.remove(0);
+		}
 	}
 	
 	public Map<TermWrapper, TermAssignment> solve()
 	{
-		List<TermAssignment> unresolved = new ArrayList<TermAssignment>();
-		for(TermWrapper key : termMap.keySet())
+		// there is nothing to resolve
+		if(varsAndMatches.size() == 0)
 		{
-			TermAssignment ve = termMap.get(key);
-			if(!(ve.getTermWrapper().getRootTerm() instanceof Variable))
+			return termMap;
+		}
+		/*
+		{
+			Set<TermWrapper> set = new HashSet<TermWrapper>();
+			for(Pair<Set<TermWrapper>, Map<TermWrapper, TermAssignment>> p : varsAndMatches)
 			{
-				unresolved.add(ve);
+				set.addAll(p.getKey());
 			}
+			new InputDialog(set, Display.getDefault());
 		}
-		for(TermAssignment ve : unresolved)
-		{
-			termMap.remove(ve.getTermWrapper());
-		}
+		*/
 		do
 		{		
 			List<TermAssignment> repeat = new ArrayList<TermAssignment>();
-			for(TermAssignment ve : unresolved)
+			for(TermAssignment ve : termAssigments)
 			{
 				AbstractReversibleOperation op = ((AbstractReversibleOperation)ve.getTermWrapper());
 				
@@ -47,9 +107,9 @@ public class VariableResolver
 					repeat.add(ve);
 				}
 			}
-			unresolved = repeat;
+			termAssigments = repeat;
 		}
-		while(unresolved.size() > 0);
+		while(termAssigments.size() > 0);
 		
 		return termMap;
 	}
