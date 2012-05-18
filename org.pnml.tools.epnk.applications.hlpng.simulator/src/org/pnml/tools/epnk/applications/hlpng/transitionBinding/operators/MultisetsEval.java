7c4391d6196f0f629748827d2c1d289cc969ee82
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-05-18 13:05:37 +0000

    MultisetsEval.java: handles a case when a NamberOf value is multiset

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/operators/MultisetsEval.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/operators/MultisetsEval.java
@@ -3,6 +3,7 @@ package org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.pnml.tools.epnk.applications.hlpng.runtime.AbstractValue;
 import org.pnml.tools.epnk.applications.hlpng.runtime.MSValue;
@@ -37,7 +38,22 @@ public class MultisetsEval implements IEvaluator
 			MSValue set = new MSValue();
 			set.setSort(operator.getSort());
 
-			set = AbstractValueMath.add(set, values.get(1), ((NumberValue)values.get(0)).getN());
+			int multiplicity = ((NumberValue)values.get(0)).getN();
+			if(values.get(1) instanceof MSValue)
+			{
+				for(Entry<AbstractValue, Integer> value : ((MSValue)values.get(1)).getValues().entrySet())
+				{
+					MSValue msValue = new MSValue();
+					msValue.setSort(value.getKey().getSort());
+					msValue.getValues().put(value.getKey(), value.getValue());
+					
+					set = AbstractValueMath.add(set, msValue, multiplicity);
+				}
+			}
+			else
+			{
+				set = AbstractValueMath.add(set, values.get(1), multiplicity);	
+			}
 
 		    return set;
 		}
