70fe98ef1e137bd12ee7b4974c834d7fcc80754d
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-06-15 07:24:53 +0000

    IFiringStrategy.java: adapted interface to the runtime state data structure

--- trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/IFiringStrategy.java
+++ trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/IFiringStrategy.java
@@ -1,12 +1,12 @@
 package org.pnml.tools.epnk.applications.hlpng.simulator;
 
 import java.util.List;
+import java.util.Map;
 
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.FiringMode;
+import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.IDWrapper;
 
 public interface IFiringStrategy
 {
-
-	public abstract FiringMode fire(List<FiringMode> modes);
-
+	public abstract FiringMode fire(Map<IDWrapper, List<FiringMode>> modes);
 }
