f124f36a551a35f4a9c9b5d64968235d8a0c0d39
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-06-15 07:26:04 +0000

    NetMarkingManager.java: visualizes the current marking

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/NetMarkingManager.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/NetMarkingManager.java
@@ -1,20 +1,12 @@
 package org.pnml.tools.epnk.applications.hlpng.simulator;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.pnml.tools.epnk.applications.hlpng.runtime.MSValue;
 import org.pnml.tools.epnk.applications.hlpng.runtime.NetMarking;
 import org.pnml.tools.epnk.applications.hlpng.runtime.PlaceMarking;
 import org.pnml.tools.epnk.applications.hlpng.runtime.TransitionMarking;
 import org.pnml.tools.epnk.applications.hlpng.runtimeStates.IRuntimeState;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.comparators.ComparisonManager;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.FiringMode;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.IDWrapper;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.operators.ReversibleOperationManager;
-import org.pnml.tools.epnk.applications.hlpng.utils.Pair;
 import org.pnml.tools.epnk.pnmlcoremodel.PetriNet;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Place;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Transition;
@@ -28,69 +20,7 @@ public class NetMarkingManager
 	{
 		this.petrinet = petrinet;
 	}
-	
-	public NetMarking createNetMarking(List<Pair<Place, MSValue>> runtimeValuesList,
-			List<FiringMode> enabledTransitions)
-	{		
-		NetMarking netMarking = new NetMarking();
-		netMarking.setNet(petrinet);
-		
-		// creates a marking for place runtime marking
-		for(Pair<Place, MSValue> pair : runtimeValuesList)
-		{
-			PlaceMarking marking  = new PlaceMarking();
-			marking.setPlace(pair.getKey());
-			marking.setMsValue(pair.getValue());
-			marking.setObject(pair.getKey());
-			
-			netMarking.getMarkings().add(marking);
-			netMarking.getObjectAnnotations().add(marking);
-		}
-		
-	    // creates a marking for enabled transitions
-		for(TransitionMarking marking : getTransitionMarkings(enabledTransitions))
-		{
-			netMarking.getMarkings().add(marking);
-			netMarking.getObjectAnnotations().add(marking);
-		}
 
-		return netMarking;
-	}
-	
-	private static List<TransitionMarking> getTransitionMarkings(List<FiringMode> modes)
-	{
-		Map<Transition, List<FiringMode>> modeMap = new HashMap<Transition, List<FiringMode>>();
-		for(FiringMode mode : modes)
-		{
-			Transition t = (Transition)mode.getTransition();
-			if(modeMap.containsKey(t))
-			{
-				modeMap.get(t).add(mode);
-			}
-			else
-			{
-				List<FiringMode> list = new ArrayList<FiringMode>();
-				list.add(mode);
-				modeMap.put(t, list);
-			}
-		}
-		
-		List<TransitionMarking> transitionMarkings = new ArrayList<TransitionMarking>();
-		
-		for(Transition transition : modeMap.keySet())
-		{
-			TransitionMarking marking = new TransitionMarking();
-    		marking.getModes().addAll(modeMap.get(transition));	
-    		
-    		marking.setTransition(transition);
-			marking.setObject(transition);
-			
-			transitionMarkings.add(marking);
-		}
-		
-		return transitionMarkings;
-	}
-	
     public NetMarking createNetMarking(IRuntimeState state)
     {       
         NetMarking netMarking = new NetMarking();
@@ -111,11 +41,20 @@ public class NetMarkingManager
         // creates a marking for enabled transitions
         for(IDWrapper wrapper : state.getTransitions())
         {
+
             TransitionMarking marking = new TransitionMarking();
-            marking.getModes().addAll(state.getFiringModes(wrapper));   
+            marking.getModes().addAll(state.getFiringModes(wrapper)); 
+           
+        	Transition transition = (Transition)wrapper.getId();
+        	// marks as fired
+        	if(state.getFiringMode() != null && 
+        			state.getFiringMode().getTransition().getId().equals(transition.getId()))
+        	{
+        		marking.setFired(true);
+        	}
             
-            marking.setTransition((Transition)wrapper.getId());
-            marking.setObject((Transition)wrapper.getId());
+            marking.setTransition(transition);
+            marking.setObject(transition);
             
             netMarking.getMarkings().add(marking);
             netMarking.getObjectAnnotations().add(marking);
