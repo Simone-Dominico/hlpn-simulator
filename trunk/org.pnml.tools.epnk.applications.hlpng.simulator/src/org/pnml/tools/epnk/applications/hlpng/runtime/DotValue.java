bef87b7a708a83df052606a2ced746b0cf815bfc
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-06-22 08:06:30 +0000

    BUG FIX: DotValue.java: implemented hashCode() & equals()

--- trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/runtime/DotValue.java
+++ trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/runtime/DotValue.java
@@ -2,9 +2,50 @@ package org.pnml.tools.epnk.applications.hlpng.runtime;
 
 public class DotValue extends AbstractValue
 {
+	private final String name = "dot";
+	
 	@Override
     public String toString()
     {
-	    return "dot";
+	    return name;
+    }
+
+	@Override
+    public int hashCode()
+    {
+	    final int prime = 31;
+	    int result = 1;
+	    result = prime * result + ((name == null) ? 0 : name.hashCode());
+	    return result;
+    }
+
+	@Override
+    public boolean equals(Object obj)
+    {
+	    if(this == obj)
+	    {
+		    return true;
+	    }
+	    if(obj == null)
+	    {
+		    return false;
+	    }
+	    if(!(obj instanceof DotValue))
+	    {
+		    return false;
+	    }
+	    DotValue other = (DotValue) obj;
+	    if(name == null)
+	    {
+		    if(other.name != null)
+		    {
+			    return false;
+		    }
+	    }
+	    else if(!name.equals(other.name))
+	    {
+		    return false;
+	    }
+	    return true;
     }
 }
