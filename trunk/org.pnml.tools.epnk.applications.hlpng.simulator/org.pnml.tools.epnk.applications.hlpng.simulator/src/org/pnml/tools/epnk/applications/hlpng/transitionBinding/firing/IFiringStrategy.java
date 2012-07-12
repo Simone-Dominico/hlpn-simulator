b3af5e7e80eb43c97d055042d6fd68694ab94574
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-07-12 07:02:41 +0000

    Moved IFiringStrategy.java & RandomFiringStrategy.java to transitionBinding/firing/

--- trunk/org.pnml.tools.epnk.applications.hlpng.simulator/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/firing/IFiringStrategy.java
+++ trunk/org.pnml.tools.epnk.applications.hlpng.simulator/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/firing/IFiringStrategy.java
@@ -1,11 +1,9 @@
-package org.pnml.tools.epnk.applications.hlpng.simulator;
+package org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing;
 
 import java.util.List;
 import java.util.Map;
 
 import org.pnml.tools.epnk.applications.hlpng.runtimeStates.IRuntimeState;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.FiringMode;
-import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.IDWrapper;
 
 public interface IFiringStrategy
 {
