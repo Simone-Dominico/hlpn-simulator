7a8a799e72126e4b06154f086c7a3991fdb52aec
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-07-19 07:43:46 +0000

    SimulatorPresentationManager.java: removed inheritance from ApplicationPresentationManager

--- trunk/org.pnml.tools.epnk.applications.hlpng.simulator/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/presentation/SimulatorPresentationManager.java
+++ trunk/org.pnml.tools.epnk.applications.hlpng.simulator/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/presentation/SimulatorPresentationManager.java
@@ -4,23 +4,31 @@ import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 import org.eclipse.swt.graphics.Font;
+import org.pnml.tools.epnk.annotations.manager.IPresentationManager;
 import org.pnml.tools.epnk.annotations.netannotations.ObjectAnnotation;
 import org.pnml.tools.epnk.applications.hlpng.presentation.marking.PlaceMarking;
 import org.pnml.tools.epnk.applications.hlpng.presentation.marking.TransitionMarking;
-import org.pnml.tools.epnk.applications.presentation.ApplicationPresentationManager;
 import org.pnml.tools.epnk.applications.presentation.IApplicationWithPresentation;
 import org.pnml.tools.epnk.applications.presentation.decorations.LabelLayer;
 import org.pnml.tools.epnk.applications.presentation.decorations.TopRightLabel;
+import org.pnml.tools.epnk.applications.presentation.popup.SelectionHandler;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Place;
 import org.pnml.tools.epnk.pntypes.hlpng.pntd.hlpngdefinition.Transition;
 
-public class SimulatorPresentationManager extends ApplicationPresentationManager
+public class SimulatorPresentationManager implements IPresentationManager
 {
+	protected SelectionHandler selectionHandler = null;
+	protected IApplicationWithPresentation simulator = null;
+	protected Font font = null;
+	
 	public SimulatorPresentationManager(IApplicationWithPresentation simulator, Font font)
 	{
-		super(simulator, font);
+		this.simulator = simulator;
+		this.selectionHandler = new SelectionHandler(simulator);
+		this.font = font;
 	}
 	
+	@Override
 	public IFigure handle(ObjectAnnotation objectAnnotation, 
 			AbstractGraphicalEditPart graphicalEditPart)
 	{
@@ -60,6 +68,12 @@ public class SimulatorPresentationManager extends ApplicationPresentationManager
 		}
 		return figure;
 	}
+	
+	@Override
+    public void notifyOwner()
+    {
+	    simulator.activate();
+    }
 }
 
 
