42478a92280ed1d73bdedcd36ece45eeac357951
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-07-24 08:40:48 +0000

    BUG FIX: MultisetsEval.java: if we have 1`(2`4++3`5)++2`(2`4) => the elements 2`4++3`5 and 2`4 are completely different

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/operators/MultisetsEval.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/transitionBinding/operators/MultisetsEval.java
@@ -21,117 +21,117 @@ import org.pnml.tools.epnk.pntypes.hlpngs.datatypes.terms.UserSort;
 
 public class MultisetsEval implements IEvaluator
 {
-	private ISortEvaluator sortEvaluator = null;
-	private RuntimeValueFactory factory = null;
-	
-	public MultisetsEval(RuntimeValueFactory factory)
-	{
-		this.factory = factory; 
-	}
-	
-	@Override
-	public IValue evaluate(Term term, EvaluationManager evaluationManager,
-			Map<TermWrapper, IValue> assignments) throws UnknownVariableException
-	{
-		Operator operator = (Operator) term;
-		List<IValue> values = new ArrayList<IValue>();
-		for(Term subterm : operator.getSubterm())
-		{
-			IValue value = evaluationManager.evaluate(subterm, evaluationManager, assignments);
-			values.add(value);
-		}
-		if(operator instanceof NumberOf)
-		{
-			IMSValue set = this.factory.createMSValue();
-			set.setSort(operator.getSort());
-
-			int multiplicity = ((NumberValue)values.get(0)).getN();
-			if(values.get(1) instanceof IMSValue)
-			{
-				for(Entry<IValue, Integer> value : ((IMSValue)values.get(1)).entrySet())
-				{
-					IMSValue msValue = this.factory.createMSValue();
-					msValue.setSort(value.getKey().getSort());
-					msValue.put(value.getKey(), value.getValue());
-					
-					set = AbstractValueMath.add(set, msValue, multiplicity, this.factory);
-				}
-			}
-			else
-			{
-				set = AbstractValueMath.add(set, values.get(1), multiplicity, this.factory);	
-			}
-
-		    return set;
-		}
-		if(operator instanceof Add)
-		{
-			IMSValue set = this.factory.createMSValue();
-			set.setSort(operator.getSort());
-			
-			for(IValue value : values)
-			{
-				IMSValue ms = (IMSValue) value;
-				set = AbstractValueMath.append(set, ms, this.factory);
-			}
-			return set;
-		}
-		if(operator instanceof Subtract)
-		{
-			IMSValue set = (IMSValue) values.get(0);
-			
-			for(int i = 1; i < values.size(); i++)
-			{
-				IMSValue ms = (IMSValue) values.get(i);
-				set = AbstractValueMath.subtract(set, ms, this.factory);
-			}
-			return set;
-		}
-		if(operator instanceof All)
-		{
-			All allOp = (All) operator;
-			return sortEvaluator.evaluate(allOp.getRefsort());
-		}
-		return null;
-	}
-
-	public void setSortEvaluator(
-            ISortEvaluator dataTypeEvaluationManager)
+    private ISortEvaluator sortEvaluator = null;
+    private RuntimeValueFactory factory = null;
+    
+    public MultisetsEval(RuntimeValueFactory factory)
     {
-    	this.sortEvaluator = dataTypeEvaluationManager;
+        this.factory = factory; 
+    }
+    
+    @Override
+    public IValue evaluate(Term term, EvaluationManager evaluationManager,
+            Map<TermWrapper, IValue> assignments) throws UnknownVariableException
+    {
+        Operator operator = (Operator) term;
+        List<IValue> values = new ArrayList<IValue>();
+        for(Term subterm : operator.getSubterm())
+        {
+            IValue value = evaluationManager.evaluate(subterm, evaluationManager, assignments);
+            values.add(value);
+        }
+        if(operator instanceof NumberOf)
+        {
+            IMSValue set = this.factory.createMSValue();
+            set.setSort(operator.getSort());
+            int multiplicity = ((NumberValue)values.get(0)).getN();
+            
+            if(values.get(1) instanceof IMSValue)
+            {
+                IMSValue msValue = this.factory.createMSValue();
+                msValue.setSort(values.get(1).getSort());
+                
+                for(Entry<IValue, Integer> value : ((IMSValue)values.get(1)).entrySet())
+                {
+                    msValue.put(value.getKey(), value.getValue());
+                }
+                set.put(msValue, multiplicity);
+            }
+            else
+            {
+                set = AbstractValueMath.add(set, values.get(1), multiplicity, this.factory);    
+            }
+
+            return set;
+        }
+        if(operator instanceof Add)
+        {
+            IMSValue set = this.factory.createMSValue();
+            set.setSort(operator.getSort());
+            
+            for(IValue value : values)
+            {
+                IMSValue ms = (IMSValue) value;
+                set = AbstractValueMath.append(set, ms, this.factory);
+            }
+            return set;
+        }
+        if(operator instanceof Subtract)
+        {
+            IMSValue set = (IMSValue) values.get(0);
+            
+            for(int i = 1; i < values.size(); i++)
+            {
+                IMSValue ms = (IMSValue) values.get(i);
+                set = AbstractValueMath.subtract(set, ms, this.factory);
+            }
+            return set;
+        }
+        if(operator instanceof All)
+        {
+            All allOp = (All) operator;
+            return sortEvaluator.evaluate(allOp.getRefsort());
+        }
+        return null;
     }
 
-	@Override
+    public void setSortEvaluator(
+            ISortEvaluator dataTypeEvaluationManager)
+    {
+        this.sortEvaluator = dataTypeEvaluationManager;
+    }
+
+    @Override
     public String validate(Object term)
     {
-		if(term instanceof NumberOf)
-		{
-			return null;
-		}
-		if(term instanceof Add)
-		{
-			return null;
-		}
-		if(term instanceof Subtract)
-		{
-			return null;
-		}
-		if(term instanceof All)
-		{
-			All all = (All)term;
-			if(sortEvaluator == null)
-			{
-				if(all.getRefsort() instanceof UserSort)
-				{
-					String name = ((UserSort)all.getRefsort()).
-							getDeclaration().getName();
-					return "User defined sort\n" + name;
-				}
-				return "all:" + all.getRefsort().getClass().getName();
-			}
-			
-			return sortEvaluator.validate(all);
-		}
-		return term.getClass().toString();
+        if(term instanceof NumberOf)
+        {
+            return null;
+        }
+        if(term instanceof Add)
+        {
+            return null;
+        }
+        if(term instanceof Subtract)
+        {
+            return null;
+        }
+        if(term instanceof All)
+        {
+            All all = (All)term;
+            if(sortEvaluator == null)
+            {
+                if(all.getRefsort() instanceof UserSort)
+                {
+                    String name = ((UserSort)all.getRefsort()).
+                            getDeclaration().getName();
+                    return "User defined sort\n" + name;
+                }
+                return "all:" + all.getRefsort().getClass().getName();
+            }
+            
+            return sortEvaluator.validate(all);
+        }
+        return term.getClass().toString();
     }
 }
