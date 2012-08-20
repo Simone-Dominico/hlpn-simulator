4b995dc0b79c974055554f4a363ba4abddd3359e
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-08-20 08:59:56 +0000

    Vis: uses VisSimulationViewController.java now (now current record highlighting)

---
+++ org.pnml.tools.epnk.applications.hlpng.visualsimulator/src/org/pnml/tools/epnk/applications/hlpng/simulator/VisSimulationViewController.java
@@ -0,0 +1,9 @@
+package org.pnml.tools.epnk.applications.hlpng.simulator;
+
+import org.pnml.tools.epnk.applications.hlpng.simulator.views.SimulationViewController;
+
+public class VisSimulationViewController extends SimulationViewController
+{
+	@Override
+	public void highlightLine(int index){}
+}
