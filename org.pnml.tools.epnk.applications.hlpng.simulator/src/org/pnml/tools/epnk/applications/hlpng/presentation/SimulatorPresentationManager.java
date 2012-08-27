155032cbc010c16886b6fca54a3a3f3eff1133df
Mindaugas.Laganeckas <Mindaugas.Laganeckas@gmail.com>
2012-08-27 07:54:39 +0000

    Sim: author text and a license

--- org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/presentation/SimulatorPresentationManager.java
+++ org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/presentation/SimulatorPresentationManager.java
@@ -1,9 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Mindaugas Laganeckas.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Mindaugas Laganeckas - initial API and implementation
+ ******************************************************************************/
 package org.pnml.tools.epnk.applications.hlpng.presentation;
 
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 import org.eclipse.swt.graphics.Font;
+import org.pnml.tools.epnk.annotations.manager.IPresentationManager;
 import org.pnml.tools.epnk.annotations.netannotations.ObjectAnnotation;
 import org.pnml.tools.epnk.applications.hlpng.presentation.decorations.LabelLayer;
 import org.pnml.tools.epnk.applications.hlpng.presentation.decorations.TopRightLabel;
@@ -11,7 +22,6 @@ import org.pnml.tools.epnk.applications.hlpng.presentation.marking.PlaceMarking;
 import org.pnml.tools.epnk.applications.hlpng.presentation.marking.TransitionMarking;
 import org.pnml.tools.epnk.applications.hlpng.presentation.popup.SelectionHandler;
 import org.pnml.tools.epnk.applications.presentation.IApplicationWithPresentation;
-import org.pnml.tools.epnk.applications.presentation.IPresentationManager;
 
 public class SimulatorPresentationManager implements IPresentationManager
 {
