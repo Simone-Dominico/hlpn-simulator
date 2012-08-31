b25b64d84d9447af2999019a47dd3437de12616b
Mindaugas.Laganeckas@gmail.com <Mindaugas.Laganeckas@gmail.com@1bab6f5d-79a2-7054-a0bc-69fc81cf2cc2>
2012-08-31 13:04:58 +0000

    RuntimeStateManager.java: a method to update transition binding (by requesting an input from a user)

--- trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/runtimeStates/IRuntimeState.java
+++ trunk/org.pnml.tools.epnk.applications.hlpng.simulator/src/org/pnml/tools/epnk/applications/hlpng/runtimeStates/IRuntimeState.java
@@ -10,13 +10,13 @@
  ******************************************************************************/
 package org.pnml.tools.epnk.applications.hlpng.runtimeStates;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.pnml.tools.epnk.applications.hlpng.runtime.IMSValue;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.FiringMode;
 import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.IDWrapper;
+import org.pnml.tools.epnk.applications.hlpng.transitionBinding.firing.TransitionCheck;
 import org.pnml.tools.epnk.pnmlcoremodel.Place;
 import org.pnml.tools.epnk.pnmlcoremodel.Transition;
 
@@ -27,16 +27,17 @@ public interface IRuntimeState
 	
 	public Map<IDWrapper, IMSValue> getValues();
 	public Map<IDWrapper, IMSValue> getClonedValues();
-	public Map<IDWrapper, List<FiringMode>> getModes();
+	public Map<IDWrapper, TransitionCheck> getModes();
 	
 	public void addValue(Place place, IMSValue value);
 	public IMSValue getValue(Place place);
 	public IMSValue getValue(IDWrapper place);
 	public Set<IDWrapper> getPlaces();
 	
-	public void addFiringModes(Transition transition, List<FiringMode> modes);
-	public List<FiringMode> getFiringModes(Transition transition);
-	public List<FiringMode> getFiringModes(IDWrapper transition);
+	public void addFiringModes(Transition transition, TransitionCheck modes);
+	public void removeFiringModes(Transition transition);
+	public TransitionCheck getFiringModes(Transition transition);
+	public TransitionCheck getFiringModes(IDWrapper transition);
 	public Set<IDWrapper> getTransitions();
 	
 	public boolean equals(Object obj);
